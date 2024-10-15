b9ae195f05f0a7a0c4fbf72d271e78765150147f
Edan Odongo <edanodongo05@gmail.com>
2024-10-15 20:45:26 +0300

    data preparation

---
+++ System/Model Architecture.py
@@ -0,0 +1,13 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+
+model = Sequential([
+    Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
+    MaxPooling2D((2, 2)),
+    Conv2D(64, (3, 3), activation='relu'),
+    MaxPooling2D((2, 2)),
+    Conv2D(128, (3, 3), activation='relu'),
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dense(10, activation='softmax')
+])
