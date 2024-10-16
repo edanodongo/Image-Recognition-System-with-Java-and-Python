bc7b53b526a517caa2c5cd060247b86421029a3c
Edan Odongo <edanodongo05@gmail.com>
2024-10-16 14:08:52 +0300

    Resize image

--- App/main.py
+++ App/main.py
@@ -13,7 +13,8 @@ img_path = 'C:/Users/Edan/Downloads/image.jpeg' # The image to classify
 #image = cv2.imread('path/to/your/image.jpg')
 img = cv2.imread(img_path)
 
-pythonCopy codeimg = cv2.resize(img, (224, 224))  # Resize the image to match the model's input size
+img = cv2.resize(img, (224, 224))  # Resize the image to match the model's input size
+
 x = image.img_to_array(img)  # Convert the image to a numpy array
 x = np.expand_dims(x, axis=0)  # Add a batch dimension
 x = preprocess_input(x)
