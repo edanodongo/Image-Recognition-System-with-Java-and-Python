f4441c7dd30614782de23b4468a2fe667dbf3ef1
Edan Odongo <edanodongo05@gmail.com>
2024-10-16 14:20:38 +0300

    predicted values

--- App/main.py
+++ App/main.py
@@ -24,4 +24,10 @@ preds = model.predict(x)
 # Decode and display predictions
 print('Predicted:', decode_predictions(preds, top=3)[0])
 
-#Predicted: [('n04254680', 'soccer_ball', 0.99946004), ('n03793489', 'mouse', 0.00015638306), ('n04540053', 'volleyball', 8.9596644e-05)]
+
+'''
+Image Recognition in Python Using Tensorflow and Keras
+Uses the pre-trained ResNet50 model to make predictions on the input image
+
+#Predicted: [('n03680355', 'Loafer', 0.90188974), ('n04200800', 'shoe_shop', 0.088929996), ('n03124043', 'cowboy_boot', 0.0030356084)]
+'''
