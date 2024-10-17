47fd3a099bc9485f59545417e39ea50273808f23
Edan Odongo <edanodongo05@gmail.com>
2024-10-17 10:05:00 +0300

    load and prepare dataset

---
+++ App/dataset.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.utils import to_categorical
+
+# Load CIFAR-10 dataset
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+# Normalize pixel values to be between 0 and 1
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+# Convert labels to one-hot encoding
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
